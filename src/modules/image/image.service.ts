16bd76f4f2ec0ba9148f1b118051f617f44a5acd
And <tednaaa@gmail.com>
2021-12-19 02:11:50 +0400

    feat(optimization for heroku): added auto deleting if max files count reached

--- src/modules/image/image.service.ts
+++ src/modules/image/image.service.ts
@@ -1,4 +1,5 @@
 import * as path from 'path';
+import * as fs from 'fs';
 import { Injectable } from '@nestjs/common';
 import {
   createFileFromBase64,
@@ -19,6 +20,35 @@ export class ImageService {
 
   create(createImageDto: CreateImageDto) {
     createFolderIfNotExists(this.folderPath);
+
+    const imagesFolder = fs.readdirSync(this.folderPath);
+    const roomsCount = imagesFolder.length;
+    const maxRoomsCount = 10;
+
+    if (roomsCount >= maxRoomsCount) {
+      let oldestImage = {
+        imageName: null,
+        lastUpdatedTimeMs: null,
+      };
+
+      imagesFolder.forEach((imageName: string) => {
+        const imageLastUpdatedTimeMs = fs.statSync(
+          path.join(this.folderPath, imageName),
+        ).ctimeMs;
+
+        if (
+          oldestImage.lastUpdatedTimeMs > imageLastUpdatedTimeMs ||
+          oldestImage.lastUpdatedTimeMs === null
+        ) {
+          oldestImage = {
+            imageName,
+            lastUpdatedTimeMs: imageLastUpdatedTimeMs,
+          };
+        }
+      });
+
+      fs.rmSync(path.join(this.folderPath, oldestImage.imageName));
+    }
 
     createFileFromBase64(
       createImageDto.image,
